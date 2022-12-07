a6361edd9078c1cb26d1b1c156ca302a831bc221
Bijoy Thomas Kuriekattil <bijoythomask@gmail.com>
2022-12-07 13:59:23 -0800

    layout for the recipe page

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable, pipe, tap } from 'rxjs';
 import { Recipe } from '../model/recipe.model';
 import { loadRecipess } from '../store/actions/recipes.actions';
 import { RecipesState } from '../store/reducers/recipes.reducer';
@@ -14,9 +15,29 @@ export class RecipesComponent implements OnInit {
   recipes$: Observable<ReadonlyArray<Recipe>> =
     this.store.select(selectRecipes);
 
-  constructor(private store: Store<RecipesState>) {}
+  coulumnCount$ = this.breakpointObserver
+    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small])
+    .pipe(
+      map((result) => {
+        const breakpoint = result.breakpoints;
+        if (breakpoint[Breakpoints.Large]) return 3;
+        else if (breakpoint[Breakpoints.Medium]) return 2;
+        return 1;
+      })
+    );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private store: Store<RecipesState>
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadRecipess());
+    this.breakpointObserver
+      .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small])
+      .pipe(tap((result) => console.log(result)))
+      .subscribe((val) => {
+        console.log(val);
+      });
   }
 }
